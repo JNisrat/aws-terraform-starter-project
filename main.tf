334ad8b2e80faba395e6399b242c98f62e6faf80
JNisrat <jahannisrat400@gmail.com>
2026-04-24 16:57:12 +1000

    UPDATE4

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ data "aws_ami" "app_ami" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
-  instance_type = "t3.nano"
+  instance_type = "var.instance_type"
 
   tags = {
     Name = "Demo-world"
@@ -24,7 +24,7 @@ resource "aws_instance" "web" {
 }
 
 resource "aws_s3_bucket" "demo_bucket" {
-  bucket = "my-terraform-demo-bucket-2026"
+  bucket = var.bucket_name
 
   tags = {
     Name = "demo-bucket"
